bda5b4b13c3b4cbdba1513c8a4cb0e5c5f6e1e13
Logain42 <alvarez.ariel.work@gmail.com>
2014-03-26 18:55:41 -0300

    Agregada creacion de un recurrent interval usando un DSL

--- src/test/scala/edu/unq/uis/planificador/EmpleadoSpec.scala
+++ src/test/scala/edu/unq/uis/planificador/EmpleadoSpec.scala
@@ -4,6 +4,7 @@ import org.joda.time.DateTimeConstants
 import org.scalatest._
 import org.scalatest.matchers.ShouldMatchers
 import com.github.nscala_time.time.Imports._
+import edu.unq.uis.planificador.RecurrentInterval._
 
 class EmpleadoSpec extends FlatSpec with ShouldMatchers {
   "Test1" should "lala" in {
@@ -28,8 +29,8 @@ class EmpleadoSpec extends FlatSpec with ShouldMatchers {
   }
 
   it should "no estar disponible si hay una restriccion especifica para un dia normalmente habilitado" in {
-    val jorge = new Empleado
-    jorge.agregarDisponibilidad(new RecurrentInterval(10, 18, DateTimeConstants.MONDAY))
+    var jorge = new Empleado
+    jorge.agregarDisponibilidad (Lunes de 10 a 18)
     jorge.agregarRestriccion( new Restriccion(new DateTime("2014-03-24T11:00")))
 
     jorge.estaDisponiblePara(new DateTime("2014-03-24T11:00")) should be (false)
