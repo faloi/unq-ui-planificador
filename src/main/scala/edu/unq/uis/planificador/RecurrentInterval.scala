3c3bdf3bb4f5b58be3f392222307267ef81ac931
Logain42 <alvarez.ariel.work@gmail.com>
2014-03-26 19:15:19 -0300

    Agregado un builder para el DSL de RecurrentInterval

--- src/main/scala/edu/unq/uis/planificador/RecurrentInterval.scala
+++ src/main/scala/edu/unq/uis/planificador/RecurrentInterval.scala
@@ -9,20 +9,4 @@ case class RecurrentInterval(inicio: Int, fin: Int, diaDeSemana: Int) {
     time.getDayOfWeek == diaDeSemana && (inicio until fin contains time.getHourOfDay)
 
   def vacio : RecurrentInterval = new RecurrentInterval(0 ,0, 0)
-
-  def a( unFin : Int) : RecurrentInterval = new RecurrentInterval(this.inicio ,unFin, this.diaDeSemana)
-
-  def de(unInicio : Int) : RecurrentInterval = new RecurrentInterval(unInicio ,this.fin, this.diaDeSemana)
-}
-
-object RecurrentInterval{
-  implicit def intToPartialInterval(d: Int) = new RecurrentInterval(d, 0, 0)
-  val Lunes : RecurrentInterval = new RecurrentInterval(0,0, DateTimeConstants.MONDAY)
-  val Martes : RecurrentInterval = new RecurrentInterval(0,0, DateTimeConstants.TUESDAY)
-  val Miercoles : RecurrentInterval = new RecurrentInterval(0,0, DateTimeConstants.WEDNESDAY)
-  val Jueves : RecurrentInterval = new RecurrentInterval(0,0, DateTimeConstants.THURSDAY)
-  val Viernes : RecurrentInterval = new RecurrentInterval(0,0, DateTimeConstants.FRIDAY)
-  val Sabado : RecurrentInterval = new RecurrentInterval(0,0, DateTimeConstants.SATURDAY)
-  val Domingo : RecurrentInterval = new RecurrentInterval(0,0, DateTimeConstants.SUNDAY)
-
 }
--- src/test/scala/edu/unq/uis/planificador/EmpleadoSpec.scala
+++ src/test/scala/edu/unq/uis/planificador/EmpleadoSpec.scala
@@ -4,7 +4,7 @@ import org.joda.time.DateTimeConstants
 import org.scalatest._
 import org.scalatest.matchers.ShouldMatchers
 import com.github.nscala_time.time.Imports._
-import edu.unq.uis.planificador.RecurrentInterval._
+import edu.unq.uis.planificador.RecurrentIntervalBuilder._
 
 class EmpleadoSpec extends FlatSpec with ShouldMatchers {
   "Test1" should "lala" in {
@@ -14,15 +14,15 @@ class EmpleadoSpec extends FlatSpec with ShouldMatchers {
 
   it should "aceptar un dia de disponibilidad" in {
     val jorge = new Empleado
-    jorge.agregarDisponibilidad(new RecurrentInterval(10, 18, DateTimeConstants.MONDAY))
+    jorge.agregarDisponibilidad (Lunes de 10 a 18)
     jorge.estaDisponiblePara(new DateTime("2014-03-24T09:00")) should be (false)
     jorge.estaDisponiblePara(new DateTime("2014-03-24T15:00")) should be (true)
   }
 
   it should "aceptar varias disponiblilidades" in {
     val jorge = new Empleado
-    jorge.agregarDisponibilidad(new RecurrentInterval(10, 18, DateTimeConstants.THURSDAY))
-    jorge.agregarDisponibilidad(new RecurrentInterval(10, 18, DateTimeConstants.MONDAY))
+    jorge.agregarDisponibilidad (Jueves de 10 a 18)
+    jorge.agregarDisponibilidad (Lunes de 10 a 18)
 
     jorge.estaDisponiblePara(new DateTime("2014-03-24T11:00")) should be (true)
     jorge.estaDisponiblePara(new DateTime("2014-03-27T15:00")) should be (true)
@@ -38,8 +38,8 @@ class EmpleadoSpec extends FlatSpec with ShouldMatchers {
 
   it should "pisar la disponibilidad anterior cuando se agrega otra para el mismo dia" in {
     val jorge = new Empleado
-    jorge.agregarDisponibilidad(new RecurrentInterval(10, 18, DateTimeConstants.MONDAY))
-    jorge.agregarDisponibilidad(new RecurrentInterval(10, 13, DateTimeConstants.MONDAY))
+    jorge.agregarDisponibilidad (Lunes de 10 a 18)
+    jorge.agregarDisponibilidad (Lunes de 10 a 13)
 
     jorge.estaDisponiblePara(new DateTime("2014-03-24T14:00")) should be (false)
   }
