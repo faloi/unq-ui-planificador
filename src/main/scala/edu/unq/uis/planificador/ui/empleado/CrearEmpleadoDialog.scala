ddc6e9cde7240ba9d29416483d0a81d0150ff726
Federico Aloi <federico.aloi@gmail.com>
2014-04-06 20:09:58 -0300

    El FormBuilder ahora se encarga de construir el EditarEmpleado

--- src/main/scala/edu/unq/uis/planificador/ui/empleado/CrearEmpleadoDialog.scala
+++ src/main/scala/edu/unq/uis/planificador/ui/empleado/CrearEmpleadoDialog.scala
@@ -2,8 +2,9 @@ package edu.unq.uis.planificador.ui.empleado
 
 import org.uqbar.arena.windows.WindowOwner
 import edu.unq.uis.planificador.domain.Empleado
+import edu.unq.uis.planificador.dependencyInjection.DevEnvironment
 
-class CrearEmpleadoDialog(owner: WindowOwner) extends EditarEmpleadoDialog(owner, new Empleado) {
+class CrearEmpleadoDialog(owner: WindowOwner) extends EditarEmpleadoDialog(owner, new Empleado) with DevEnvironment {
   override val title = "Nuevo empleado"
 
   override def executeTask() {
--- src/main/scala/edu/unq/uis/planificador/ui/empleado/EditarEmpleadoDialog.scala
+++ src/main/scala/edu/unq/uis/planificador/ui/empleado/EditarEmpleadoDialog.scala
@@ -2,12 +2,11 @@ package edu.unq.uis.planificador.ui.empleado
 
 import org.uqbar.arena.windows.{Dialog, WindowOwner}
 import org.uqbar.arena.widgets._
-import org.uqbar.arena.layout.VerticalLayout
 import org.uqbar.arena.aop.potm.Function
 import edu.unq.uis.planificador.domain.Empleado
-import edu.unq.uis.planificador.ui.widgets.FieldCreator
+import edu.unq.uis.planificador.ui.widgets.FormBuilder
 
-class EditarEmpleadoDialog(owner: WindowOwner, model: Empleado) extends Dialog[Empleado](owner, model) with FieldCreator {
+class EditarEmpleadoDialog(owner: WindowOwner, model: Empleado) extends Dialog[Empleado](owner, model) with FormBuilder {
   override def addActions(actionsPanel: Panel) {
     new Button(actionsPanel)
       .setCaption("Guardar")
@@ -21,15 +20,10 @@ class EditarEmpleadoDialog(owner: WindowOwner, model: Empleado) extends Dialog[E
 
   def title = "Editando " + getModelObject.nombreCompleto
 
-  override def createFormPanel(mainPanel: Panel) {
-    setTitle(title)
-    mainPanel.setLayout(new VerticalLayout)
-
-    createFields(mainPanel, Map(
-      "Nombre" -> "nombre",
-      "Apellido" -> "apellido",
-      "Legajo" -> "legajo")
-    )
-  }
+  val fields = Map(
+    "Nombre" -> "nombre",
+    "Apellido" -> "apellido",
+    "Legajo" -> "legajo"
+  )
 }
 
